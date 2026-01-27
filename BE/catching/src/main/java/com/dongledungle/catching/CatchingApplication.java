10477ab08671a4cbcc08473e36fee86514f31026
Joochan Kim <voidness12@gmail.com>
2026-01-27 21:28:29 +0900

    [BE] Minor: Timezone 설정
    Set default time zone to Asia/Seoul in main method.

--- BE/catching/src/main/java/com/dongledungle/catching/CatchingApplication.java
+++ BE/catching/src/main/java/com/dongledungle/catching/CatchingApplication.java
@@ -2,20 +2,12 @@ package com.dongledungle.catching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
-/**
- * Catching 애플리케이션 메인 클래스
- * Spring Boot 애플리케이션 시작점
- */
 @SpringBootApplication
 public class CatchingApplication {
-
-	/**
-	 * 애플리케이션 시작 메서드
-	 *
-	 * @param args 커맨드 라인 인자
-	 */
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(CatchingApplication.class, args);
 	}
 }
