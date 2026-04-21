6a42e1c3f8be9c83f56412ac49d6cbffcc829be9
PoloCeleste <voidness12@gmail.com>
2026-04-21 15:51:05 +0900

    [BE] Minor: Safety 필터 처리 추가

---
+++ BE/catching/src/main/java/com/dongledungle/catching/analysis/exception/SafetyBlockedException.java
@@ -0,0 +1,7 @@
+package com.dongledungle.catching.analysis.exception;
+
+public class SafetyBlockedException extends RuntimeException {
+    public SafetyBlockedException(String message) {
+        super(message);
+    }
+}
